f3c2e77c034e3148a366491009394af2591291f0
zhanghao_py <zhanghao_py@sina.com>
2013-08-29 00:10:00 +0800

    加入 passport验证

---
+++ src/main/java/com/baidu/foo/ssh/core/config/SystemConfig.java
@@ -0,0 +1,8 @@
+package com.baidu.foo.ssh.core.config;
+
+public class SystemConfig {
+
+	public static final String CLIENTID = "z9xnxYDK4Z13a6TWO8HBNcZd";
+	public static final String CLIENTSECRET = "iQMi2YYnGgQwR9dfDlSKHfgwPUFQlrBC";
+	public static final String REDIRECTURI = "http://127.0.0.1:8080/index.jsp";
+}
