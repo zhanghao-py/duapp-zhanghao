bb86e7f13d215937222e6e4eb77b6bf6ba366f70
zhanghao <zhanghao_py@sina.com>
2013-09-01 23:58:33 +0800

    集成spring-sec（Ongoing）

--- src/main/java/com/baidu/foo/ssh/core/web/filter/UserAuthFilter.java
+++ src/main/java/com/baidu/foo/ssh/core/web/filter/UserAuthFilter.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -14,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.springframework.web.filter.GenericFilterBean;
 
 import com.baidu.api.Baidu;
 import com.baidu.api.BaiduApiException;
@@ -24,59 +23,53 @@ import com.baidu.api.store.BaiduCookieStore;
 import com.baidu.api.store.BaiduStore;
 import com.baidu.foo.ssh.core.config.SystemConfig;
 
-public class UserAuthFilter implements Filter {
+public class UserAuthFilter extends GenericFilterBean {
+
 
 	private static Logger log = Logger.getLogger(UserAuthFilter.class);
-	
+
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
-		
+
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
-		
+
 		String clientId = SystemConfig.CLIENTID;
 		String clientSecret = SystemConfig.CLIENTSECRET;
 		String redirectUri = req.getRequestURL().toString();
-		
-        BaiduStore store = new BaiduCookieStore(clientId, req, res);
-        Session session = store.getSession();
-        
-        // 当前已登录
-        if (session != null) {
-        	// 获取当前登陆者
-        	User user = session.getUser();
-        	log.info(user.getUname());
-        	chain.doFilter(request, response);
-        	
-        } else {
-        
-	        Baidu baidu = null;
-	        
-	        try {
-	            baidu = new Baidu(clientId, clientSecret, redirectUri, store, req);
-	            String state = baidu.getState();
-	            Map<String, String> params = new HashMap<String, String>();
-	            params.put("state", state);
-	            String authorizeUrl = baidu.getBaiduOAuth2Service().getAuthorizeUrl(params);
-	            res.sendRedirect(authorizeUrl);
-	        } catch (BaiduOAuthException e) {
-	            log.warn("BaiduOAuthException ", e);
-	        } catch (BaiduApiException e) {
-	            log.warn("BaiduApiException ", e);
-	        }
-        }
-        
+
+		BaiduStore store = new BaiduCookieStore(clientId, req, res);
+		Session session = store.getSession();
+
+		// 当前已登录
+		if (session != null) {
+			// 获取当前登陆者
+			User user = session.getUser();
+			log.info(user.getUname());
+			chain.doFilter(request, response);
+
+		} else {
+
+			Baidu baidu = null;
+
+			try {
+				baidu = new Baidu(clientId, clientSecret, redirectUri, store,
+						req);
+				String state = baidu.getState();
+				Map<String, String> params = new HashMap<String, String>();
+				params.put("state", state);
+				String authorizeUrl = baidu.getBaiduOAuth2Service()
+						.getAuthorizeUrl(params);
+				res.sendRedirect(authorizeUrl);
+			} catch (BaiduOAuthException e) {
+				log.warn("BaiduOAuthException ", e);
+			} catch (BaiduApiException e) {
+				log.warn("BaiduApiException ", e);
+			}
+		}
+
 	}
 
-	@Override
-	public void destroy() {
-		
-	}
-	
-	@Override
-	public void init(FilterConfig config) throws ServletException {
-		
-	}
 
 }
